8b80719b9a724970708f55f39d78f47b4243bf9e
Oleh Stasiv <o.stasiv@sweet.tv>
2025-06-30 12:03:53 +0300

    Added tests for publishers

--- TransactionsTestTaskTests/BitcoinRateServiceTests.swift
+++ TransactionsTestTaskTests/BitcoinRateServiceTests.swift
@@ -5,26 +5,31 @@
 //  Created by Oleg Stasiw on 30.06.2025.
 //
 
+import Foundation
 import XCTest
+import Combine
 @testable import TransactionsTestTask
 
 class BitcoinRateServiceTests: XCTestCase {
   
   var bitcoinRateService: BitcoinRateService!
   var mockBitcoinRateCacheManager: MockBitcoinRateCacheManager!
+  var cancellables: Set<AnyCancellable>!
   
   override func setUp() {
+    super.setUp()
     let configuration = URLSessionConfiguration.default
     configuration.protocolClasses = [MockURLProtocol.self]
     let urlSession = URLSession.init(configuration: configuration)
     mockBitcoinRateCacheManager = MockBitcoinRateCacheManager()
     bitcoinRateService = BitcoinRateServiceImpl(bitcoinRateCacheManager: mockBitcoinRateCacheManager, session: urlSession)
-    super.setUp()
+    cancellables = Set<AnyCancellable>()
   }
   
   override func tearDown() {
     bitcoinRateService = nil
     mockBitcoinRateCacheManager = nil
+    cancellables = nil
     super.tearDown()
   }
   
@@ -131,5 +136,82 @@ class BitcoinRateServiceTests: XCTestCase {
       expectation.fulfill()
     }
     wait(for: [expectation], timeout: 1)
+  }
+  
+  func testFetchCurrentBTCPricePublishesToPublisher() {
+    let expectation = XCTestExpectation(description: "Publisher receives rate")
+    var publishedRate: BitcoinRate?
+    
+    bitcoinRateService.rateUpdatePublisher
+      .sink { rate in
+        publishedRate = rate
+        expectation.fulfill()
+      }
+      .store(in: &cancellables)
+    
+    let jsonString = """
+      {
+        "price": "5000"
+      }
+    """
+    
+    let data = jsonString.data(using: .utf8)
+    
+    MockURLProtocol.loadingHandler = { request in
+      let response = HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: nil)!
+      return (data, response, nil)
+    }
+    
+    bitcoinRateService.fetchCurrentBTCPrice { _ in }
+    
+    wait(for: [expectation], timeout: 1)
+    if let rate = publishedRate {
+      XCTAssertNotNil(rate)
+      XCTAssertFalse(rate.price.isEmpty)
+      XCTAssertEqual(rate.price, "5000")
+      XCTAssertTrue(Double(rate.price) != nil)
+    }
+  }
+  
+  func testRateUpdatePublisherWithMultipleSubscribers() {
+    let expectation1 = XCTestExpectation(description: "First subscriber receives rate")
+    let expectation2 = XCTestExpectation(description: "Second subscriber receives rate")
+    var publishedRate1: BitcoinRate?
+    var publishedRate2: BitcoinRate?
+    
+    bitcoinRateService.rateUpdatePublisher
+      .sink { rate in
+        publishedRate1 = rate
+        expectation1.fulfill()
+      }
+      .store(in: &cancellables)
+    
+    bitcoinRateService.rateUpdatePublisher
+      .sink { rate in
+        publishedRate2 = rate
+        expectation2.fulfill()
+      }
+      .store(in: &cancellables)
+    
+    
+    let jsonString = """
+      {
+        "price": "5000"
+      }
+    """
+    
+    let data = jsonString.data(using: .utf8)
+    
+    MockURLProtocol.loadingHandler = { request in
+      let response = HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: nil)!
+      return (data, response, nil)
+    }
+    
+    bitcoinRateService.fetchCurrentBTCPrice { _ in }
+    
+    wait(for: [expectation1, expectation2], timeout: 1)
+    if let rate1 = publishedRate1, let rate2 = publishedRate2 {
+      XCTAssertEqual(rate1.price, rate2.price)
+    }
   }
 }
